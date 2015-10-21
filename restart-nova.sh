9371b90fc3a13e719a5c425063e3bb21e95c7556
melon-li <lookat119@163.com>
2015-10-21 16:12:20 +0800

    add ctl-net neutron config file

--- restart-nova.sh
+++ restart-nova.sh
@@ -1,1 +1,5 @@
-for i in api cert compute novncproxy scheduler consoleauth  conductor;do service openstack-nova-$i restart ;done
+
+for i in api cert novncproxy scheduler consoleauth  conductor
+do 
+	service nova-$i  $1
+done
